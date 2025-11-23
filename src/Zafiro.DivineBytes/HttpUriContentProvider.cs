37a0b9abe517c150e8c9890e5ab8f2068a6c709b
José Manuel Nieto <superjmn@outlook.com>
2025-11-23 11:51:18 +0100

    Avoid buffering HTTP responses in DivineBytes (#105)
    * Avoid buffering HTTP responses in DivineBytes
    
    * Fix byte source reuse for HTTP downloads

--- src/Zafiro.DivineBytes/HttpUriContentProvider.cs
+++ src/Zafiro.DivineBytes/HttpUriContentProvider.cs
@@ -1,3 +1,4 @@
+using System.Net.Http;
 using CSharpFunctionalExtensions;
 
 namespace Zafiro.DivineBytes;
@@ -38,21 +39,20 @@ public class HttpUriContentProvider : IUriContentProvider, IDisposable
                 return Result.Failure<IByteSource>($"Unsupported URI scheme: {uri.Scheme}. Only HTTP and HTTPS are supported.");
             }
 
-            // Validate that the URI is accessible without creating a ByteSource yet
-            using var testResponse = await httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
-
-            if (!testResponse.IsSuccessStatusCode)
-            {
-                return Result.Failure<IByteSource>($"Error downloading from {uri}: {testResponse.StatusCode} - {testResponse.ReasonPhrase}");
-            }
-
-            // Create the ByteSource using Zafiro's HttpResponseMessageStream
             var byteSource = ByteSource.FromAsyncStreamFactory(async () =>
             {
-                var response = await httpClient.GetAsync(uri, cancellationToken);
-                response.EnsureSuccessStatusCode();
+                var response = await httpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
 
-                return await HttpResponseMessageStream.Create(response);
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    return await HttpResponseMessageStream.Create(response);
+                }
+                catch
+                {
+                    response.Dispose();
+                    throw;
+                }
             });
 
             return Result.Success(byteSource);
